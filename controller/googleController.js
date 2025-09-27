ff40e329063189606edbd608aadf275940390b80
Ferdy Hahan Pradana <ferdyhahan5@gmail.com>
2025-09-28 06:43:55 +0700

    delete: cleanup

--- controller/googleController.js
+++ controller/googleController.js
@@ -4,28 +4,6 @@ import {
   parseGoogleResults,
 } from "../utils/helper.js";
 
-export const fetchGoogle1 = async (req, res) => {
-  try {
-    const query = req.query.q || 'intext:"polinema"';
-    const start = parseInt(req.query.start || "0", 10);
-
-    const html = await fetchGoogleHTML(query, start);
-
-    if (checkGoogleCaptcha(html)) {
-      return res.status(429).json({
-        error:
-          "Whoops, Google detected something unusual. Copy the cookie again!",
-      });
-    }
-
-    res.set("Content-Type", "text/html");
-    res.send(html);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-};
-
 export const fetchGoogle = async (req, res) => {
   try {
     const query = req.query.q || 'intext:"polinema"';
